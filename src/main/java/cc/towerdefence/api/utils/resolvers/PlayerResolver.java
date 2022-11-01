72602722317a1647fdc5e878c3d32a64eab4f492
Zak Shearman <34372536+ZakShearman@users.noreply.github.com>
2022-11-01 18:32:42 +0000

    Improvements for the player resolver

--- src/main/java/cc/towerdefence/api/utils/resolvers/PlayerResolver.java
+++ src/main/java/cc/towerdefence/api/utils/resolvers/PlayerResolver.java
@@ -14,7 +14,9 @@ import java.time.Duration;
 import java.util.UUID;
 import java.util.concurrent.ForkJoinPool;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
+@SuppressWarnings("unused")
 public class PlayerResolver {
     private static final Cache<String, CachedMcPlayer> USERNAME_TO_PLAYER_CACHE = Caffeine.newBuilder()
             .maximumSize(1000)
@@ -22,18 +24,29 @@ public class PlayerResolver {
             .build();
 
     private static McPlayerGrpc.McPlayerFutureStub playerService;
+    // The alternative should be used as a server software specific option to avoid calling the mc-player service if they're on the same server.
+    private static Function<String, CachedMcPlayer> alternativeUsernameResolver;
 
-    public static void setPlayerService(@NotNull McPlayerGrpc.McPlayerFutureStub playerService) {
+    public static void setPlayerService(@NotNull McPlayerGrpc.McPlayerFutureStub playerService, Function<String, CachedMcPlayer> alternativeUsernameResolver) {
         if (PlayerResolver.playerService != null) throw new IllegalStateException("Player service already set");
         PlayerResolver.playerService = playerService;
+        PlayerResolver.alternativeUsernameResolver = alternativeUsernameResolver;
     }
 
-    public static void retrievePlayerUuid(String username, Consumer<CachedMcPlayer> callback, Consumer<Status> errorCallback) {
-        CachedMcPlayer cacheResult = USERNAME_TO_PLAYER_CACHE.getIfPresent(username);
+    public static void retrievePlayerData(String username, Consumer<CachedMcPlayer> callback, Consumer<Status> errorCallback) {
+        String usernameLowercase = username.toLowerCase();
+
+        CachedMcPlayer alternativeOption = alternativeUsernameResolver.apply(usernameLowercase);
+        if (alternativeOption != null) {
+            callback.accept(alternativeOption);
+            return;
+        }
+
+        CachedMcPlayer cacheResult = USERNAME_TO_PLAYER_CACHE.getIfPresent(usernameLowercase);
         if (cacheResult != null) callback.accept(cacheResult);
 
-        requestMcPlayer(username, uuid -> {
-            USERNAME_TO_PLAYER_CACHE.put(username, uuid);
+        requestMcPlayer(usernameLowercase, uuid -> {
+            USERNAME_TO_PLAYER_CACHE.put(usernameLowercase, uuid);
             callback.accept(uuid);
         }, errorCallback);
     }
