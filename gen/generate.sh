223bccc70c0e554ea8fef7ed28121dff8ab404a9
Zak Shearman <34372536+ZakShearman@users.noreply.github.com>
2022-11-26 12:28:44 +0000

    generate gRPC server files

--- gen/generate.sh
+++ gen/generate.sh
@@ -8,5 +8,5 @@ mkdir -p go
 find ../src/proto -name '*.proto' -print0 | while IFS= read -r -d '' file
 do
   echo "Generating protos for $file"
-  protoc -I ../src/proto --go_out=go "$file"
+  protoc -I ../src/proto --go_out=go --go-grpc_out=go "$file"
 done
